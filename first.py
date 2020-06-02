a6ca450158f3a8f6ce93e2305019cd1164b3041f
alexeybliznyuk <66327586+alexeybliznyuk@users.noreply.github.com>
2020-06-02 20:58:55 +0200

    Update first.py
    FIx all letters. Mb have bug with variables.

--- first.py
+++ first.py
@@ -2,213 +2,142 @@ print("Эта программа шифрует по ОДНОЙ букве на 
 print("Чтобы закончить шифрование напишите end")
 print("В начале шифра добовляется 000 и в конце")
 print("После каждой буквы идёт цыфра 2")
-asd = 000
-str(asd)
-st = 000
-str(st)
-two = 2
-str(two)
+asd = "000"
+st = "000"
+two = "2"
 while 2 == 2:
     a = input("Назовите букву:")
     if a == "а":
-        aa = 1
-        aa = True
-        str(aa)
+        aa = "1"
+        
+        
         st = st + aa + two
-        aa = False
+        
         
     elif a == "б":
-        bb = 10
-        bb = True
-        str(bb)
+        bb = "10"
+        
+        
         st = st + bb + two
-        bb = False
+        
     elif a == "в":
-        vv = 11
-        vv = True
-        str(vv)
+        vv = "11"
+        
+        
         st = st + vv + two
-        vv = False
+        
     elif a == "г":
-        gg = 100
-        gg = True
-        str(gg)
+        gg = "100"
+        
+        
         st = st + gg + two
-        gg = False
+        
     elif a == "д":
-        dd = 101
-        dd = True
-        str(dd)
+        dd = "101"
+        
+        
         st = st + dd + two
-        dd = False
+        
     elif a == "е":
-        ye = 110
-        ye = True
-        str(ye)
+        ye = "110"
+        
+        
         st = st + ye + two
-        ye = False
+        
     elif a == "ё":
-        yo = 111
-        yo = True
-        str(yo)
+        yo = "111"
+        
+        
         st = st + yo + two
-        yo = False
+        
     elif a == "ж":
-        zhe = 1000
-        zhe = True
-        str(zhe)
+        zhe = "1000"
+        
+        
         st = st + zhe + two
-        zhe = False
+        
     elif a == "з":
-        ze = 1001
-        ze = True
-        str(ze)
+        ze = "1001"
+        
+        
         st = st + ze + two
-        ze = False
+        
     elif a == "и":
-        ii = 1010
-        ii = True
-        str(ii)
+        ii = "1010"
         st = st + ii + two
-        ii = False
+        
     elif a == "й":
-        ikr = 1011
-        ikr = True
-        str(ikr)
+        ikr = "1011"
         st = st + ikr + two
-        ikr = False
     elif a == "к":
-        ka = 1100
-        ka = True
-        str(ka)
+        ka = "1100"
+        
         st = st + ka + two
-        ka = False
+        
     elif a == "л":
-        ll = 1101
-        ll = True
-        str(ll)
-        st = st + ka + two
-        ll = False
+        ll = "1101"
+        st = st + ll + two
     elif a == "м":
-        em = 1110
-        em = True
-        str(em)
+        em = "1110"
         st = st + em + two
-        em = False
     elif a == "н":
-        en = 1111
-        en = True
-        str(en)
+        en = "1111"
         st = st + en + two
-        en = False
+        
     elif a == "о":
-        oo = 10000
-        oo = True
-        str(oo)
+        oo = "10000"
         st = st + oo + two
-        oo = False
     elif a == "п":
-        pe = 10001
-        pe = True
-        str(pe)
+        pe = "10001"
         st = st + pe + two
-        pe = False
     elif a == "р":
-        re = 10010
-        re = True
-        str(re)
+        re = "10010"
         st = st + re + two
-        re = False
     elif a == "с":
-        ss = 10011
-        ss = True
-        str(ss)
+        ss = "10011"
         st = st + ss + two
-        ss = False
     elif a == "т":
-        tt = 10100
-        tt = True
-        str(tt)
+        tt = "10100"
         st = st + tt + two
-        st = False
     elif a == "у":
-        uu = 10101
-        uu = True
-        str(uu)
+        uu = "10101"
         st = st + uu + two
-        uu = False
     elif a == "ф":
-        ff = 10110
-        ff = True
-        str(ff)
-        st = st + uu + two
-        ff = False
+        ff = "10110"
+        st = st + ff + two  
     elif a == "х":
-        hh = 10111
-        hh = True
-        str(hh)
-        st = st + hh + two
-        hh = False
+        hh = "10111"
+        st = st + hh + two   
     elif a == "ц":
-        zshe = 11000
-        zshe = True
-        str(zshe)
+        zshe = "11000"
         st = st + zshe + two
-        zshe = False
     elif a == "ч":
-        che = 11001
-        che = True
-        str(che)
+        che = "11001"
         st = st + che + two
-        che = False
     elif a == "ш":
-        sh = 11010
-        sh = True
-        str(sh)
+        sh = "11010"
         st = st + sh + two
-        sh = False
     elif a == "щ":
-        sha = 11011
-        sha = True
-        str(sha)
+        sha = "11011"
         st = st + sha + two
-        sha = False
     elif a == "ъ":
-        tverd = 11100 
-        tverd = True
-        str(tverd)
+        tverd = "11100" 
         st = st + tverd + two
-        tverd = False
     elif a == "ы":
-        qwerty = 11101
-        qwerty = True
-        str(qwerty)
+        qwerty = "11101"
         st = st + qwerty + two
-        qwerty = False
     elif a == "ь":
-        mye = 11110
-        mye = True
-        str(mye)
+        mye = "11110"
         st = st + mye + two
-        mye = False
     elif a == "э":
-        eee = 11111
-        eee = True
-        str(eee)
+        eee = "11111"
         st = st + eee + two
-        eee = False
     elif a == "ю":
-        yu = 100000
-        yu = True
-        str(yu)
+        yu = "100000"
         st = st + yu + two
-        yu = False
     elif a == "я":
-        ya = 100001
-        ya = True
-        str(ya)
+        ya = "100001"
         st = st + ya + two
-        ya = False
     elif a == "end":
         break
 print(st + asd)
